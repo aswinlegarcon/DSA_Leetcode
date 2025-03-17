c889b5af59b3b664536527488868424356299dfe
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-03-17 14:11:20 +0530

    Time: 5 ms (55.23%) | Memory: 44.8 MB (88.18%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> numAndInd = new HashMap<>();
         int result[] = new int[2];
-        for(int i=0;i<nums.length;i++)
+        for(int i =0;i<nums.length;i++)
         {
-            for(int j=i+1;j<nums.length;j++)
+            int numToFind = target - nums[i];
+            if(numAndInd.containsKey(numToFind))
             {
-                if(nums[i] + nums[j] == target)
-                {
-                    result[0] = i;
-                    result[1] = j; 
-                }
+                result[0] = i;
+                result[1] = numAndInd.get(numToFind);
             }
+            numAndInd.put(nums[i],i);
         }
         return result;
     }
