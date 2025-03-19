eec13b02f7a5b4fe3b3d84071d77a0a431e85847
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-03-19 23:32:15 +0530

    Time: 18 ms (62.20%) | Memory: 43.9 MB (64.82%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,33 +1,42 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        HashSet<List<Integer>> arr = new HashSet<>();
         int n= nums.length;
         for(int a=0;a<n;a++)
         {
+            if(a>0 && nums[a] == nums[a-1]) continue;
             for(int b=a+1;b<n;b++)
             {
-                HashSet<Long> checkD = new HashSet<>();
-                for(int c = b+1;c<n;c++)
+                if(b>a+1 && nums[b] == nums[b-1]) continue;
+                int c = b+1;
+                int d = n-1;
+                while(c<d)
                 {
-                    long sum = nums[a];
+                    long sum = nums[a]; // should not add in single line so that it can exceed int
                     sum += nums[b];
                     sum += nums[c];
-                    long d = target - sum;
-                    if(checkD.contains(d))
+                    sum += nums[d];
+                    if(sum == target)
                     {
-                        List<Integer> temp = new ArrayList<>(Arrays.asList(nums[a],nums[b],nums[c],(int)d));
-                        Collections.sort(temp);
-                        arr.add(temp);
+                        List<Integer> temp = new ArrayList<>(Arrays.asList(nums[a],nums[b],nums[c],nums[d]));
+                        result.add(temp);
+                        c++;
+                        d--;
+                        while(c<d && nums[c] == nums[c-1]) c++;
+                        while(d>c && nums[d] == nums[d+1]) d--;
+
                     }
-                    checkD.add((long)nums[c]);
+                    else if(sum > target)
+                    {
+                        d--;
+                    }
+                    else
+                    {
+                        c++;
+                    }
                 }
             }
-        }
-
-        for(var lis:arr)
-        {
-            result.add(lis);
         }
         return result;
     }
