87b768bfc8f771e188daa95ba03c8c2a4facc7dc
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-04-22 23:08:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (16.14%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double result = 1.0;
+        long power = n;
+        if(power<0)
+        {
+            power = -1 * power; // convert negative to positive
+        }
+        while(power>0)
+        {
+            if(power%2==0)
+            {
+                x = x*x;
+                power = power/2;
+            }
+            else if(power%2==1)
+            {
+                result = result * x;
+                power = power-1;
+            }
+        } 
+        if(n<0) return (double)1.0/(double)result; // if given power is negative
+        return result;
+    }
+}
