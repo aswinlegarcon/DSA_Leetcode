dddd70ecae2df176f1cbcbccb9a64189999bf6a7
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-04-29 12:19:21 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (48.93%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,26 +1,13 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        double num = n;
-        if(n==1)
+        // power of 2's binary will always be like 10000,100,100000000
+        // if we unset the rightmost bit and the value becomes 0 then it is a power of 2
+        if(n<=0) return false;
+        int unset = n & (n-1); // unsetting the rightmost bit
+        if(unset==0)
         {
             return true;
         }
-        if(n%2!=0)
-        {
-            return false;
-        }
-        return helpPower(num,0);
-    }
-    public boolean helpPower(double n,int i)
-    {
-        if(n==1.0)
-        {
-            return true;
-        }
-        else if(n==0)
-        {
-            return false;
-        }
-        return helpPower(n/2,i+1);
+        return false;
     }
 }
