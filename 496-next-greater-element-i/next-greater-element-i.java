9297475e0fde94dad4c90080166bef833fa41e33
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-05-04 18:57:42 +0530

    Time: 5 ms (17.23%) | Memory: 43.8 MB (59.76%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,30 +1,31 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int nge[] = new int[nums1.length];
-        HashMap<Integer,Integer> numAndInd = new HashMap<>();
-        for(int i=0;i<nums2.length;i++)
+        int nge[] = new int[nums2.length];
+        Stack<Integer> st = new Stack<>(); 
+        int n2 = nums2.length;
+        for(int i=n2-1;i>=0;i--)
         {
-            numAndInd.put(nums2[i],i);
+            while(!st.empty() && st.peek() <= nums2[i])
+            {
+                st.pop();
+            }
+            if(st.empty()) nge[i] = -1;
+            else nge[i] = st.peek();
+            st.push(nums2[i]);
         }
+
+        int ans[] = new int[nums1.length];
         for(int i=0;i<nums1.length;i++)
         {
-            boolean yes = false;
-            int num = nums1[i];
-            int index = numAndInd.get(num);
-            for(int j=index;j<nums2.length;j++)
+            for(int j=0;j<nums2.length;j++)
             {
-                if(nums2[j] > num)
+                if(nums2[j] == nums1[i])
                 {
-                    yes = true;
-                    nge[i] = nums2[j];
+                    ans[i] = nge[j];
                     break;
                 }
             }
-            if(!yes)
-            {
-                nge[i] = -1;
-            }
         }
-        return nge;
+        return ans;
     }
 }
