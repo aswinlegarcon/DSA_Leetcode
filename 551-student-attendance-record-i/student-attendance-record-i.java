8c3a5703772a923e8f72015e4b96034593984db4
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-05-22 12:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (49.79%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.java
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -1,32 +1,5 @@
 class Solution {
     public boolean checkRecord(String s) {
-        int cntA = 0;
-        int cntL = 0;
-        boolean check = false;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)=='L') 
-            {
-                cntL++;
-                if(cntL==3) check = true;
-            }
-            else if(s.charAt(i)=='A') 
-            {
-                cntL = 0;
-                cntA++;
-            }
-            else
-            {
-                cntL = 0;
-            }
-            
-                       
-        }
-
-        if(cntA < 2 && !check)
-        {
-            return true;
-        }
-        return false;
+        return s.indexOf("A")==s.lastIndexOf("A") && !s.contains("LLL");
     }
 }
