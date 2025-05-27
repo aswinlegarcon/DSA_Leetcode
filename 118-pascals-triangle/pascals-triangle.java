cef48fe8633e8486e464b8fc3752e83fdeb9f581
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-05-27 15:19:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (14.76%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> temp = new ArrayList<>();
+        for(int i=1;i<=numRows;i++)
+        {
+            temp.add(pascal(i));
+        }
+    return temp;
+    }
+
+        private List<Integer> pascal(int row)
+        {
+            List<Integer> temp = new ArrayList<>();
+            int ans = 1;
+            temp.add(ans);
+            for(int i=1;i<row;i++)
+            {
+                ans = ans * (row-i) / i;
+                temp.add(ans);
+            }
+            return temp;
+        }
+    
+}
