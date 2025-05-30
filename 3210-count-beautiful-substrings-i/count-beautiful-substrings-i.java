5e3ef43bc39f424c238ed8baa34ebd9506e63c17
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-05-30 14:44:01 +0530

    Time: 51 ms (83.11%) | Memory: 42.2 MB (59.46%) - LeetSync

--- 3210-count-beautiful-substrings-i/count-beautiful-substrings-i.java
+++ 3210-count-beautiful-substrings-i/count-beautiful-substrings-i.java
@@ -1,16 +1,7 @@
 class Solution {
     public int beautifulSubstrings(String s, int k) {
         int cnt = 0;
-        int n = s.length();
-       
-        HashSet<Character> set = new HashSet<>();
-        set.add('a');
-        set.add('e');
-        set.add('i');
-        set.add('o');
-        set.add('u');
-
-        
+        int n = s.length();    
         for(int i=0;i<n;i++)
         {
             int vowCnt = 0;
@@ -18,7 +9,7 @@ class Solution {
             for(int j=i;j<n;j++)
             {
                 char c = s.charAt(j);
-                if(set.contains(c)) vowCnt++;
+                if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u') vowCnt++;
                 else csnCnt++;
                 if(vowCnt==csnCnt && (vowCnt*csnCnt)%k==0) cnt++;
             }
