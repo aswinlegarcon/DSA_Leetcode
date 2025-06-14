1f3eae8ad20e187fb7ce6db1d2e4bc789cf2b878
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-06-14 22:28:20 +0530

    Time: 6 ms (48.60%) | Memory: 42.9 MB (91.44%) - LeetSync

---
+++ 179-largest-number/largest-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        ArrayList<String> temp = new ArrayList<>();
+        for(int num:nums)
+        {
+            temp.add(String.valueOf(num));
+        }
+
+        Collections.sort(temp,(a,b)-> (b+a).compareTo(a+b));
+
+        StringBuilder sb = new StringBuilder();
+        for(String s:temp)
+        {
+            sb.append(s);
+        }
+
+        return sb.toString().charAt(0)=='0'?"0":sb.toString();
+    }
+}
