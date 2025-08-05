11aa3fab5a58aefadd1ef3697e827d30a015d078
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-08-05 21:37:29 +0530

    Time: 1 ms (21.87%) | Memory: 41.4 MB (67.18%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean rotateString(String s, String goal) 
+    {
+        
+        StringBuilder sb = new StringBuilder(s);
+        for(int i=0;i<s.length();i++)
+        {
+
+            if(sb.toString().equals(goal)) return true;
+            char first = sb.charAt(0);
+            sb.deleteCharAt(0);
+            sb.append(first);
+        }
+
+        return false;
+    }
+}
