cb02a0eccd1cddf72da72019986d10591a04d0dd
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-08-08 23:25:40 +0530

    Time: 3 ms (81.99%) | Memory: 43 MB (26.41%) - LeetSync

--- 2235-capitalize-the-title/capitalize-the-title.java
+++ 2235-capitalize-the-title/capitalize-the-title.java
@@ -4,22 +4,31 @@ class Solution {
         StringBuilder ans = new StringBuilder();
         for(String s:arr)
         {
+
             int len = s.length();
+
             if(len<3)
+
             {
+
                 String temp = s.toLowerCase();
+                
                 ans.append(temp);
                 ans.append(" ");
             }
             else
             {
+
                 String temp = "";
                 temp += s.substring(0,1).toUpperCase();
                 temp += s.substring(1,len).toLowerCase();
                 ans.append(temp);
                 ans.append(" ");
             }
+
         }
+
+
         return ans.toString().trim();
     }
 }
