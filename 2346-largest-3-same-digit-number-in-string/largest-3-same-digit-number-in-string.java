1c88a13f19cf83235fa72c7adc0a5a05eed9cf25
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-08-22 23:17:34 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (77.36%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -4,11 +4,13 @@ class Solution {
 
         String nums[] = {"999","888","777","666","555","444","333","222","111","000"};
 
+
         for(String s : nums)
         {
             if(num.contains(s)) return s;
         }
         
+
         return "";
     }
     
