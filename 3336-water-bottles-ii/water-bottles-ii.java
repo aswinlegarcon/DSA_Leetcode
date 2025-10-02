9abf02e581522ce8520323d5ef1e28194916ce5c
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-10-02 21:15:20 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (57.42%) - LeetSync

---
+++ 3336-water-bottles-ii/water-bottles-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int total = numBottles;
+        int empty = numBottles;
+        numBottles = 0;
+        while(numExchange <= empty + numBottles)
+        {
+            if(numExchange <= empty)
+            {
+                numBottles += 1;
+                empty -= numExchange;
+                numExchange++;
+            }
+            else
+            {
+                total += numBottles;
+                empty += numBottles;
+                numBottles = 0;
+            }
+        }
+        total += numBottles;
+        return total;
+    }
+}
