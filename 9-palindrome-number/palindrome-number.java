c1f83ff3dd1158a1ad5fcfa7ab531be1bdc98334
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-10-10 22:19:20 +0530

    Time: 7 ms (13.14%) | Memory: 44 MB (77.20%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,11 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        
         String numAsString = String.valueOf(x);
         StringBuilder reverseNum = new StringBuilder(numAsString);
+
         if(numAsString.equals(reverseNum.reverse().toString()))
         {
             return true;
         }
         return false;
     }
+
 }
