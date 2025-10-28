c766542749c061b6cd33073943945e4e09d4492a
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-10-28 21:28:52 +0530

    Time: 2 ms (97.56%) | Memory: 47 MB (11.43%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int max = -1;
+        int n = arr.length;
+        for(int i=n-1;i>=0;i--)
+        {
+            int temp = arr[i];
+            arr[i] = max;
+            max = Math.max(temp,max);
+        }
+        return arr;
+    }
+}
