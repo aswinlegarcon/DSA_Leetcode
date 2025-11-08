8a226437b66b0117d49d879779a439802834b667
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-11-08 23:13:17 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (5.75%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,65 +1,52 @@
 class Solution {
-
-    public int lowerBound(int arr[],int x)
-    {
-        int n = arr.length;
-        int right = n-1;
-        int left = 0;
-        int lb = n;
-        while(left<=right)
-        {
-            int mid = (left+right) /2;
-            if(arr[mid] >= x)
-            {
-                lb = mid;
-                right = mid-1;
-            }
-            else
-            {
-                left = mid+1;
-            }
-        }
-        return lb;
-    }
-
-    public int upperBound(int arr[],int x)
-    {
-        int n = arr.length;
-        int right = n-1;
-        int left = 0;
-        int ub = n;
-        while(left<=right)
-        {
-            int mid = (left+right) /2;
-            if(arr[mid] > x)
-            {
-                ub = mid;
-                right = mid-1;
-            }
-            else
-            {
-                left = mid+1;
-            }
-        }
-        return ub;
-    }
         
     public int[] searchRange(int[] nums, int target) {
         int first = -1;
         int last = -1;
         int n = nums.length;
         int result[] = new int[2]; 
-        int lb = lowerBound(nums,target);
-        if(lb==n || nums[lb] != target) 
+        int left = 0;
+        int right = n-1;
+
+        while(left<=right)
         {
-            result[0] = -1;
-            result[1] = -1;
+            int mid = (left+right) / 2;
+            if(nums[mid] == target)
+            {
+                first = mid;
+                right = mid-1;
+            }
+            else if(nums[mid] > target)
+            {
+                right = mid-1;
+            }
+            else
+            {
+                left = mid+1;
+            }
         }
-        else
+        left = 0;
+        right = n-1;
+        
+        while(left<=right)
         {
-            result[0] = lb;
-            result[1] = upperBound(nums,target)-1;
+            int mid = (left+right) / 2;
+            if(nums[mid] == target)
+            {
+                last = mid;
+                left = mid+1;
+            }
+            else if(nums[mid] > target)
+            {
+                right = mid-1;
+            }
+            else
+            {
+                left = mid+1;
+            }
         }
+        result[0] = first;
+        result[1] = last;
         return result;
     }
 }
