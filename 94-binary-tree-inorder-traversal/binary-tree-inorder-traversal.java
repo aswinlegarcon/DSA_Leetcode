91a782ec8c23105beb90fcf8370c40db1c832d0e
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-11-20 22:39:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (26.53%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -14,17 +14,22 @@
  * }
  */
 class Solution {
+
     public List<Integer> inorderTraversal(TreeNode root) {
+
         return inorder(root,new ArrayList<>());
+        
     }
 
     public List<Integer> inorder(TreeNode root,ArrayList<Integer> list)
     {
+
         if(root==null) return list;
 
         inorder(root.left,list);
         list.add(root.val);
         inorder(root.right,list);
         return list;
+
     }
 }
