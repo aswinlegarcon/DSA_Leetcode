a4dbe631644ddddce6f75bd07d5a1bd85ba2cb3d
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-11-25 23:13:27 +0530

    Time: 2 ms (51.90%) | Memory: 44.8 MB (13.99%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -4,16 +4,17 @@ class Solution {
         generate(ans,"",0,0,n);
         return ans;
     }
-
-    public void generate(List<String> ans, String s, int open,int close,int max)
+    private void generate(List<String> ans, String s, int open,int close,int max)
     {
-        if(s.length() == 2*max)
+
+        if(s.length() == 2 * max)
         {
             ans.add(s);
             return;
         }
-
         if(open < max) generate(ans,s+'(',open+1,close,max);
         if(close < open) generate(ans,s+')',open,close+1,max);
+
     }
+    
 }
