66a8ba05e05bf84cd9819c49f814da43b287e298
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-12-25 18:13:36 +0530

    Time: 3 ms (87.86%) | Memory: 48.2 MB (35.22%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+
         int n = matrix.length;
         int m = matrix[0].length;
         int rows = 0;
         int cols = m-1;
+
         while(rows<n && cols>=0)
         {
             if(matrix[rows][cols] == target)
@@ -19,6 +21,8 @@ class Solution {
                 cols--;
             }
         }
+
         return false;
     }
+    
 }
