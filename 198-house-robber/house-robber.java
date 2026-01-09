05be2a9717ff663d1aecacdfd3a0a008e3127557
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-01-10 00:48:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (7.55%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,12 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
+
         int[] dp = new int[nums.length];
         Arrays.fill(dp,-1);
         return helper(nums,nums.length-1,dp);
+
     }
 
     private int helper(int[] arr,int ind,int[] dp)
     {
+
         if(ind==0) return arr[0];
         if(ind<0) return 0;
         if(dp[ind]!=-1) return dp[ind];
@@ -14,5 +17,6 @@ class Solution {
         int notPick = 0 + helper(arr,ind-1,dp);
         dp[ind] = Math.max(pick,notPick);
         return Math.max(pick,notPick);
+        
     }
 }
