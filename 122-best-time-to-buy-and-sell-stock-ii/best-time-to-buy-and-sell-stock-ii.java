46e0975938675563663a9ee8f329cc3e642ffa85
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-01-10 23:49:49 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (23.09%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int profit=0;
+
+        int i=1;
+
+        while(i<prices.length){
+            if(prices[i]>prices[i-1]){
+                profit+=prices[i]-prices[i-1];
+            }
+            i++;
+        }
+        return profit;
+    }
+
+}
