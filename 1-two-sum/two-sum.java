5c375360260b96148ffdbb4981f2cd648955dac0
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-02-07 22:19:17 +0530

    Rename result array to ans in twoSum method

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,17 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        
         HashMap<Integer,Integer> numAndInd = new HashMap<>();
-        int result[] = new int[2];
+        int ans[] = new int[2];
+        
         for(int i =0;i<nums.length;i++)
         {
             int numToFind = target - nums[i];
             if(numAndInd.containsKey(numToFind))
             {
-                result[0] = i;
-                result[1] = numAndInd.get(numToFind);
+                ans[0] = i;
+                ans[1] = numAndInd.get(numToFind);
             }
             numAndInd.put(nums[i],i);
         }
-        return result;
+        
+        return ans;
     }
 }
