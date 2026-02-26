38befb02c6b6ff84bbd95237a9d4fde446e48eb5
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-02-26 23:44:24 +0530

    Time: 21 ms (97.31%) | Memory: 57.7 MB (74.73%) - LeetSync

---
+++ 1930-maximum-number-of-consecutive-values-you-can-make/maximum-number-of-consecutive-values-you-can-make.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int getMaximumConsecutive(int[] c) {
+    Arrays.sort(c);
+    int max_val = 1;
+    for (int i = 0; i < c.length && c[i] <= max_val; ++i)
+        max_val += c[i];
+    return max_val;        
+}
+}
