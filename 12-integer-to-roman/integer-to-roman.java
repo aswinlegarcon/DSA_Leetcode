2956bdb411c61aae98d1ae44199e7e4d3ed781aa
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-03-25 23:47:51 +0530

    Time: 4 ms (33.76%) | Memory: 46.3 MB (86.08%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,9 +1,11 @@
 class Solution {
     public String intToRoman(int num) {
+
         int[] n = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] s = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int i =0;
         String str = new String(); 
+        
         while (num>0){
             if (num>=n[i]){
                 str=str+s[i];
@@ -13,5 +15,6 @@ class Solution {
             }
         }
         return str;
+
     }
 }
