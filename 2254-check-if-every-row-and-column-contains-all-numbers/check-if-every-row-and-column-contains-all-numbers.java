eea1fc3eb003f1df1209f8bed5e99c30b237b0d7
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-04-03 23:31:44 +0530

    Time: 3 ms (97.35%) | Memory: 48.4 MB (14.90%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.java
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.java
@@ -1,36 +1,33 @@
 class Solution {
     public boolean checkValid(int[][] matrix) {
         int n = matrix.length;
-        int[] row = new int[n+1];
-        int[] col = new int[n+1];
-        for(int i = 0 ; i< n ; i++){
-            for (int j = 0 ; j< n ; j++){
-                int a = matrix[i][j];
-                int b = matrix[j][i];
-                
-                if ( i == j ){
-                    if (row[a]==0 && col[b]==0){
-                        row[a]=1;
-                        col[b]=1;
-                    }else{
-                        return false;
-                    }
-                }else{
-                    if (row[a]==0){
-                        row[a]=1;
-                    }else{
-                        return false;
-                    }
-                    if (col[b]==0){
-                        col[b]=1;
-                    }else{
-                        return false;
-                    }
-                }
 
+        for(int i=0;i<n;i++)
+        {
+            if(!check(matrix[i])) return false;
+            int col[] = new int[n];
+            for(int j=0;j<n;j++)
+            {
+                col[j] = matrix[j][i];
             }
-            row= new int[n+1];
-            col= new int[n+1];
+            if(!check(col)) return false;
+        }
+        
+        return true;
+    }
+
+    private boolean check(int[] arr)
+    {
+        int n = arr.length;
+        int hash[] = new int[n+1];
+        for(int i=0;i<n;i++)
+        {
+            hash[arr[i]]++;
+        }
+
+        for(int i=1;i<=n;i++)
+        {
+            if(hash[i]!=1) return false;
         }
         return true;
     }
