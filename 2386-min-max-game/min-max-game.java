c6e2744bde5f6533f5d0a71662b60e1aa1ff5005
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-04-12 21:26:59 +0530

    Time: 1 ms (68.39%) | Memory: 45.3 MB (10.89%) - LeetSync

---
+++ 2386-min-max-game/min-max-game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minMaxGame(int[] nums) {
+        int n = nums.length;
+        if(n==1) return nums[0];
+        
+        while(n>1)
+        {
+            int newArr[] = new int[n/2];
+            for(int i=0;i<n/2;i++)
+            {
+                if(i%2==0) 
+                {
+                    newArr[i] = Math.min(nums[2*i], nums[2*i+1]);
+                }
+                else
+                {
+                    newArr[i] = Math.max(nums[2*i], nums[2*i+1]);
+                }
+            }
+            nums = newArr;
+            n = nums.length;
+        }
+        return nums[0];
+    }
+}
