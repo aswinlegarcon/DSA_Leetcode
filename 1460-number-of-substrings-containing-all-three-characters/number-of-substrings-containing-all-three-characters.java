41430f2e5ebc595ba8e1b89992122789107a1b1c
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-04-23 23:36:45 +0530

    Time: 10 ms (95.48%) | Memory: 46.2 MB (63.47%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,9 +1,11 @@
 class Solution {
+    
     public int numberOfSubstrings(String s) {
         int a = -1;
         int b = -1;
         int c = -1;
         int count = 0;
+
         for(int i=0;i<s.length();i++)
         {
             switch(s.charAt(i))
@@ -18,11 +20,13 @@ class Solution {
                     c = i;
                     break;
             }
+
             if(a!=-1 && b!=-1 && c!=-1)
             {
                 count = count + (1+Math.min(a,Math.min(b,c)));
             }
         }
+
         return count;
     }
 }
