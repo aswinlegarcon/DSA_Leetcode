60ebbd2693ae82bf37b071fb02ea136a5a5f4f0c
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-05-24 19:52:30 +0530

    Time: 30 ms (27.16%) | Memory: 46.7 MB (7.45%) - LeetSync

--- 1844-maximum-number-of-balls-in-a-box/maximum-number-of-balls-in-a-box.java
+++ 1844-maximum-number-of-balls-in-a-box/maximum-number-of-balls-in-a-box.java
@@ -1,20 +1,26 @@
 class Solution {
     public int countBalls(int lowLimit, int highLimit) {
-        int hash[] = new int[46]; // largest num is 99999 = 45
-        int max = 0;
         
+        HashMap<Integer,Integer> map = new HashMap<>();
+
         for(int i=lowLimit;i<=highLimit;i++)
         {
             int sum = getSum(i);
-            hash[sum]++;
-            max = Math.max(max,hash[sum]);
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
 
+        int max = 0;
+        for(var key:map.keySet())
+        {
+            max = Math.max(map.get(key),max);
+        }
         return max;
     }
 
+
     private int getSum(int num)
     {
+
         int sum = 0;
         while(num>0)
         {
@@ -23,4 +29,5 @@ class Solution {
         }
         return sum;
     }
+
 }
