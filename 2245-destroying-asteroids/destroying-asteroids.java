5522925576c45180a591e4ae71dd4c71a3116652
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-05-31 22:21:06 +0530

    Time: 25 ms (93.30%) | Memory: 107.6 MB (16.40%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        int n = asteroids.length;
+        Arrays.sort(asteroids);
+        long massOfPlanet = mass;
+        for(int i=0;i<n;i++)
+        { 
+            if(massOfPlanet >= asteroids[i])
+            {
+                massOfPlanet += asteroids[i];
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
