55e9a638b91a02e1f3bb9bb3be80a86e5927bbbe
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-06-02 23:10:34 +0530

    Time: 7 ms (3.97%) | Memory: 46.9 MB (39.29%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,7 +1,72 @@
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
         int min = Integer.MAX_VALUE;
-        for(int i=0;i<landStartTime.length;i++)
+        
+        int n = landStartTime.length;
+        int m = waterStartTime.length;
+        int i = 0;
+        while(i < n && i < m)
+        {
+            int landRideEnd = landStartTime[i] + landDuration[i];
+            int waterRideEnd = waterStartTime[i] + waterDuration[i];
+            int j = 0;
+            while(j < n && j < m)
+            {
+                int waterStart = waterStartTime[j];
+                int landStart = landStartTime[j];
+                if(waterStart > landRideEnd)
+                {
+                    int waitTime = waterStart - landRideEnd;
+                    min = Math.min(min, landRideEnd + waitTime + waterDuration[j]);
+                }
+                else
+                {
+                    min = Math.min(min, landRideEnd + waterDuration[j]);
+                }
+                if(landStart > waterRideEnd)
+                {
+                    int waitTime = landStart - waterRideEnd;
+                    min = Math.min(min, waterRideEnd + waitTime + landDuration[j]);
+                }
+                else{
+                    min = Math.min(min, waterRideEnd + landDuration[j]);
+                }
+                j++;
+            }
+
+            while(j < m)
+            {
+                int waterStart = waterStartTime[j];
+                if(waterStart > landRideEnd)
+                {
+                    int waitTime = waterStart - landRideEnd;
+                    min = Math.min(min, landRideEnd + waitTime + waterDuration[j]);
+                }
+                else
+                {
+                    min = Math.min(min, landRideEnd + waterDuration[j]);
+                }
+                j++;
+            }
+
+            while(j < n)
+            {
+                int landStart = landStartTime[j];
+                if(landStart > waterRideEnd)
+                {
+                    int waitTime = landStart - waterRideEnd;
+                    min = Math.min(min, waterRideEnd + waitTime + landDuration[j]);
+                }
+                else{
+                    min = Math.min(min, waterRideEnd + landDuration[j]);
+                }
+                j++;
+            }
+            i++;
+
+        }
+
+        while(i < n)
         {
             int landRideEnd = landStartTime[i] + landDuration[i];
             for(int j=0;j<waterStartTime.length;j++)
@@ -17,9 +82,10 @@ class Solution {
                     min = Math.min(min, landRideEnd + waterDuration[j]);
                 }
             }
+            i++;
         }
 
-        for(int i=0;i<waterStartTime.length;i++)
+        while(i < m)
         {
             int waterRideEnd = waterStartTime[i] + waterDuration[i];
             for(int j=0;j<landStartTime.length;j++)
@@ -34,7 +100,9 @@ class Solution {
                     min = Math.min(min, waterRideEnd + landDuration[j]);
                 }
             }
+            i++;
         }
+
         return min;
         
     }
