6d468e7680621b4584fda451cc6d9850785229c6
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-06-24 12:26:16 +0530

    Time: 7 ms (97.40%) | Memory: 46.9 MB (78.44%) - LeetSync

--- 1260-day-of-the-year/day-of-the-year.java
+++ 1260-day-of-the-year/day-of-the-year.java
@@ -1,9 +1,22 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 class Solution {
     public int dayOfYear(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate dateObj = LocalDate.parse(date,formatter);
-        return dateObj.getDayOfYear();
+        int year = Integer.parseInt(date.substring(0,4));
+        int month = Integer.parseInt(date.substring(5,7));
+        int day = Integer.parseInt(date.substring(8,10));
+        int[] months = {31,28,31,30,31,30,31,31,30,31,30,31};
+        int totalDays = day;
+        if(isLeapYear(year) && month > 2) totalDays++;
+
+        for(int i=0;i<month-1;i++)
+        {
+            totalDays += months[i];
+        } 
+        return totalDays;
+    }
+
+    private boolean isLeapYear(int year)
+    {
+        if(year % 400 == 0 || year % 4 == 0 && year%100 != 0) return true;
+        return false;
     }
 }
