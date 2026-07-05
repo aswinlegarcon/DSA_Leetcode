e44ccbb6f1895249636c53bb19032a54b83087e8
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-07-05 23:09:51 +0530

    Time: 7 ms (77.14%) | Memory: 45.4 MB (68.98%) - LeetSync

--- 1483-rank-teams-by-votes/rank-teams-by-votes.java
+++ 1483-rank-teams-by-votes/rank-teams-by-votes.java
@@ -1,41 +1,41 @@
 class Solution {
     public String rankTeams(String[] votes) {
-
+        int n = votes.length;
         int m = votes[0].length();
 
-        int[][] count = new int[26][m];
+        int[][] hash = new int[26][m];
 
-        for (String vote : votes) {
-            for (int i = 0; i < m; i++) {
-                char c = vote.charAt(i);
-                count[c - 'A'][i]++;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                char c = votes[i].charAt(j);
+                hash[c-'A'][j]++;
             }
         }
 
-        Character[] teams = new Character[m];
-
-        for (int i = 0; i < m; i++) {
-            teams[i] = votes[0].charAt(i);
+        Character[] team = new Character[m];
+        for(int i=0;i<m;i++)
+        {
+            team[i] = votes[0].charAt(i);
         }
-
-        Arrays.sort(teams, (a, b) -> {
-
-            for (int i = 0; i < m; i++) {
-                if (count[a - 'A'][i] != count[b - 'A'][i]) {
-                    return count[b - 'A'][i] - count[a - 'A'][i];
+        Arrays.sort(team, (a,b) -> {
+            for(int i=0;i<m;i++)
+            {
+                if(hash[a-'A'][i] != hash[b-'A'][i])
+                {
+                    return hash[b-'A'][i] - hash[a-'A'][i];
                 }
             }
-
             return a - b;
         });
 
         StringBuilder sb = new StringBuilder();
-
-        for (char c : teams)
+        for(int i=0;i<m;i++)
         {
-            sb.append(c);
+            sb.append(team[i]);
         }
-
         return sb.toString();
+
     }
 }
