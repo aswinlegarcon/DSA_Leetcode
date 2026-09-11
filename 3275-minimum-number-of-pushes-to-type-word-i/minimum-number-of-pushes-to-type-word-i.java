8c82dd43609c6b4ff7d8e3294d94cc93ea63c490
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-09-11 23:54:04 +0530

    Time: 2 ms (11.73%) | Memory: 43.8 MB (5.12%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,7 +1,5 @@
 class Solution {
-    
     public int minimumPushes(String word) {
-
         int cnt = 0;
         int n = word.length();
         if(n<=8) return n;
@@ -35,8 +33,6 @@ class Solution {
                 cnt += 4;
             }
         }
-
         return cnt;
-
     }
 }
