386e773fa55276d8477221160e5824e983eb0b60
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2026-09-16 22:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (88.94%) - LeetSync

--- 3320-maximum-number-of-operations-with-the-same-score-i/maximum-number-of-operations-with-the-same-score-i.java
+++ 3320-maximum-number-of-operations-with-the-same-score-i/maximum-number-of-operations-with-the-same-score-i.java
@@ -1,5 +1,7 @@
 class Solution {
+    
     public int maxOperations(int[] nums) {
+
         int operation = 1;
         int score = nums[0] + nums[1];
         for (int i = 2; i < nums.length - 1; i += 2) {
@@ -10,6 +12,7 @@ class Solution {
                 break;
             }
         }
+
         return operation;
     }
 }
